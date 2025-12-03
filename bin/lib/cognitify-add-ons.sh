58f19e7745ff2ee31acb4a3160ea4257e549e318
Ramon Brooker <rbrooker@aeo3.io>
2025-12-03 14:48:04 -0500

    used cursor to perform code review and corrections

--- bin/lib/cognitify-add-ons.sh
+++ bin/lib/cognitify-add-ons.sh
@@ -2,4 +2,6 @@
 
 ## (C) 2023
 ## Ramon Brooker <rbrooker@aeo3.io>
-#shellcheck disable=SC2034
+
+# Variables defined here are exported for use in other scripts
+# shellcheck disable=SC2034  # Unused variables are exported for external use
--- bin/lib/variables.sh
+++ bin/lib/variables.sh
@@ -5,13 +5,14 @@
 
 # Variables for Cognitify and Computed Variables
 
-#shellcheck disable=SC2034
+# Variables defined here are exported for use in other scripts
+# shellcheck disable=SC2034  # Unused variables are exported for external use
 
 CURRENT_VERSION="$(cat "${ROOT_DIR}"/version)"
 
 VERSION_FILE="/etc/bash.bashrc.d/.cognitify-version"
 
-if [[ -x "${VERSION_FILE}" ]]; then 
+if [[ -f "${VERSION_FILE}" ]] && [[ -r "${VERSION_FILE}" ]]; then 
     INSTALLED_VERSION=$(cat "${VERSION_FILE}")
 else
     INSTALLED_VERSION="none"
@@ -22,8 +23,8 @@ if [ "${INSTALLED_VERSION}" = "none" ]; then
 else 
     if [[ "${CURRENT_VERSION}" = "${INSTALLED_VERSION}" ]]; then 
         ACTION="no action"
-    elif  false ; then 
-        # Add case where current is newer then older 
+    elif [[ "$(printf '%s\n' "${CURRENT_VERSION}" "${INSTALLED_VERSION}" | sort -V | head -n1)" != "${CURRENT_VERSION}" ]]; then 
+        # Installed version is newer than current
         ACTION="installed is newer"
     else   
         ACTION="update"

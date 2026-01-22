cdc706686b2eb4352e6dc40dde07b5917d93ee59
Ramon Brooker <rbrooker@aeo3.io>
2026-01-22 09:40:30 -0500

    pruned everything not related to prompt pretty, with some aliases

--- bin/lib/cognitify-add-ons.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-## (C) 2026
-## Ramon Brooker <rbrooker@aeo3.io>
-
-# Variables defined here are exported for use in other scripts
-# shellcheck disable=SC2034  # Unused variables are exported for external use
